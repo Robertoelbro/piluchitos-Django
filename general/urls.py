e920db5554702c20a7afded7ec46c728b08a6b37
Potute <134301032+Potute@users.noreply.github.com>
2024-07-15 15:03:13 -0400

    Add files via upload

--- general/urls.py
+++ general/urls.py
@@ -7,4 +7,9 @@ from . import views
 
 urlpatterns = [
     path('i1', views.i1, name='i1'),
+    path('nosotros',views.nosotros,name='nosotros'),
+    path('productos',views.productos,name='productos'),
+    path('sesion',views.sesion,name='sesion'),
+    path('creacion',views.creacion,name='creacion'),
+    path('inicio_adm',views.inicio_adm,name='inicio_adm'),
 ]
--- general/views.py
+++ general/views.py
@@ -5,3 +5,23 @@ from django.shortcuts import render
 def i1(request):
     context={}
     return render(request, 'i1.html', context)
+
+def nosotros(request):
+    context={}
+    return render(request, 'nosotros.html', context)
+
+def sesion(request):
+    context={}
+    return render(request, 'sesion.html', context)
+
+def productos(request):
+    context={}
+    return render(request, 'productos.html', context)
+
+def inicio_adm(request):
+    context={}
+    return render(request, 'inicio_adm.html', context)
+
+def creacion(request):
+    context={}
+    return render(request, 'creacion.html', context)
